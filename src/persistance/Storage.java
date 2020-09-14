12bab6257fc8176088892365bf264e3793c2b482
Patrik-Hasselbad <phasselblad@hotmail.com>
2020-09-14 23:14:20 +0200

    Finishing for today, added some empty methods

--- src/persistance/Storage.java
+++ src/persistance/Storage.java
@@ -1,8 +1,46 @@
 package persistance;
 
+import model.Member;
+
+import java.util.ArrayList;
+
 /**
  *
  * @author ph222ue (Patrik Hasselblad)
  */
 public class Storage {
+    private ArrayList<Member> members;
+
+    public static void addMember() {
+
+    }
+
+    public static void editMember() {
+
+    }
+
+    public static void deleteMember() {
+
+    }
+
+    public static void addBoat() {
+
+    }
+
+    public static void editBoat() {
+
+    }
+
+    public static void deleteBoat() {
+
+    }
+
+    public static void loadData() {
+
+    }
+
+    public static void saveData() {
+
+    }
+    
 }
