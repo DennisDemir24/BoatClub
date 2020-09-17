314948a2a023bdcce903aec9fe26a725c1cdb00a
Nicklas Hansson <nicke_h90@hotmail.com>
2020-09-17 21:58:41 +0200

    Rebuilt some Controller logic to better fit MVC model

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,7 @@
-import controller.BoatController;
-import controller.MainController;
-import model.Boat;
-import model.Member;
-import model.Type;
-import persistance.Storage;
-import view.MainView;
 
+import controller.MainController;
 import java.io.IOException;
-import java.util.ArrayList;
+
 
 /**
  * Main class
@@ -16,56 +10,7 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-
-        Storage saveMember = new Storage();
-        BoatController boatController = new BoatController();
-        MainView mainView = new MainView();
-        MainController main = new MainController(mainView);
-        System.out.println(saveMember.registrySize());
-
-        // Testing to add members and boats
-//        Member patrik = new Member("Patrik", "Hasselblad", 83040700, 0);
-        Member patrik = new Member();
-        patrik.setFirstName("Patrik");
-        patrik.setLastName("Hasselblad");
-        patrik.setSocialSec(83040700);
-
-        saveMember.addMember(patrik);
-        Boat patsBoat = new Boat(Type.KAYAK, 3.0, patrik.getMemberId());
-        Boat patsSecondBoat = new Boat(Type.MOTORSAILER, 4.3, patrik.getMemberId());
-        boatController.createBoat(patrik, patsBoat);
-        boatController.createBoat(patrik, patsSecondBoat);
-
-        Member test = new Member();
-
-        // Fetching a members boat list
-        ArrayList<Boat> boatList = patrik.getBoatList();
-        System.out.println("My boats: " );
-        for (int i = 0 ; i < boatList.size(); i++) {
-            System.out.println(boatList.get(i).getType() + " " + boatList.get(i).getLength());
-        }
-        System.out.println();
-
-        // Adding another member
-//        Member rutger = new Member("Rutger", "Hauer", 453423, 0);
-//        saveMember.addMember(rutger);
-
-        System.out.println("Registry size: " + saveMember.registrySize());
-        System.out.println();
-
-        // Printing out member list
-        Member temp = saveMember.getMember(patrik.getMemberId());
-        System.out.println(temp.getFirstName() + " " + temp.getLastName() + "\n" + temp.getSocialSec() + "\n" + temp.getMemberId());
-        System.out.println();
-
-        // Edits a member (Early stage since everything is hardcoded atm)
-        saveMember.editMember(patrik.getMemberId());
-
-        Member temp1 = saveMember.getMember(patrik.getMemberId());
-        System.out.println(temp1.getFirstName() + " " + temp1.getLastName() + "\n" + temp1.getSocialSec() + "\n" + temp1.getMemberId());
+        MainController main = new MainController();
         main.startMenu();
-
-        // Förmodligen bäst att spara strax innan avslut.
-        saveMember.saveData();
     }
 }
--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -18,71 +18,90 @@ public class MainController {
     private BoatController boatController;
     private Storage storage;
     private MemberView memView;
-    private Scanner sc = new Scanner(System.in);;
+    private Scanner sc;
 
-    public MainController(MainView mainView) throws IOException {
-        this.mainView = mainView;
+    public MainController() throws IOException {
+        this.mainView = new MainView();
+        this.memView = new MemberView();
         this.memberController = new MemberController();
-        //this.boatController = new BoatController();
+        this.storage = new Storage();
+        // this.boatController = new BoatController();
     }
 
-    public MainController() {}
 
     /**
      * Start Application method
      * @author dd222gc (Dennis Demir)
      */
     public void startMenu() {
-        mainView.displayWelcomeMessage();
+        sc = new Scanner(System.in);
+
         int input;
        do {
+           mainView.displayWelcomeMessage();
            input = sc.nextInt();
 
            if (input == 0) {
+               sc.close();
                break;
            }
 
            switch (input) {
                case 1:
+                   sc.close();
                    break;
                case 2:
+                   sc.close();
                    break;
                case 3:
+                   createNewMember();
                    break;
                case 4:
+                   sc.close();
                    break;
                case 5:
+                   sc.close();
                    break;
                case 6:
+                   sc.close();
                    break;
                case 7:
+                   sc.close();
                    break;
                case 8:
+                   sc.close();
                    break;
                case 9:
+                   sc.close();
                    break;
                case 10:
                    mainView.displayBoatInfo();
                    break;
-               case 0:
-                   break;
                default:
                    System.out.println("Please use a number to make a choice");
            }
-       } while (input != 0);
+       } while (true);
     }
 
+    /**
+     * Method for creating a member
+     * @author dd222gc (Dennis Demir) & nh222mr (Nicklas Hansson)
+     */
     public void createNewMember() {
-        Member member = new Member();
+        sc = new Scanner(System.in);
 
         memView.displayMemberName();
-        String memName = sc.nextLine();
+        String memFirstName = sc.nextLine();
+
+        memView.displayMemberName();
+        String memLastName = sc.nextLine();
 
         memView.displayMemberPersonalNumber();
         int personalNum = sc.nextInt();
 
-        member.setFirstName(memName);
-        member.setSocialSec(personalNum);
+        memberController.create(memFirstName, memLastName, personalNum);
+
+        storage.saveData();
 
     }
 }
